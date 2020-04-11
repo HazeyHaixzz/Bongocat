70da96b9d72b69d9a640c4b15e7ac212a6fd18ea
HazeyHaixzz <57023139+HazeyHaixzz@users.noreply.github.com>
2020-04-11 22:32:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const bot = new Client();
 
 const PREFIX = 'e!';
  
+const cheerio = require('cheerio');
+
+const request = require('request');
+
 
 bot.on('ready', () =>{
 
@@ -124,5 +128,119 @@ bot.on('message', msg =>{
         msg.channel.sendMessage('neko neko :3');
     }
 })
+
+
+bot.on('message', message => {
+
+ 
+
+    let args =message.content.substring(PREFIX.length).split(" ");
+
+ 
+
+    switch (args[0]) {
+        case 'meme':
+        image(message);
+        break;
+    } 
+});
+
+ 
+
+function image(message){
+
+ 
+
+    var options = {
+
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "meme",
+
+        method: "GET",
+
+        headers: {
+
+            "Accept": "text/html",
+
+            "User-Agent": "Chrome"
+
+        }
+
+    };
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+    request(options, function(error, response,responseBody) {
+
+        if (error) {
+
+            return;
+
+        }
+
+ 
+
+ 
+
+        $ = cheerio.load(responseBody);
+
+ 
+
+ 
+
+        var links = $(".image a.link");
+
+ 
+
+        var urls = newArray(links.length).fill(0).map((v, i) =>links.eq(i).attr("href"));
+
+       
+
+        console.log(urls);
+
+ 
+
+        if (!urls.length) {
+
+           
+
+            return;
+
+        }
+
+ 
+
+        // Send result
+
+        message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
+
+    });
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+}
+
+
 
 bot.login(process.env.BOT_TOKEN);
